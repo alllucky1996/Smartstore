d067de344ab4a5b98f91623e731d9171fedab801
dungna <itfa.ahihi@gmail.com>
2023-08-02 02:21:51 +0700

    Change view in component

--- src/Smartstore.Modules/Smartstore.DevTools/Module.cs
+++ src/Smartstore.Modules/Smartstore.DevTools/Module.cs
@@ -4,10 +4,14 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using Smartstore.Web.Modelling;
+using System.Data;
+using System.IO;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Smartstore.Engine.Modularity;
 using Smartstore.Http;
+using Smartstore.IO;
+using Smartstore.Utilities;
 
 namespace Smartstore.DevTools
 {
@@ -22,6 +26,7 @@ namespace Smartstore.DevTools
         {
             await TrySaveSettingsAsync<ProfilerSettings>();
             await ImportLanguageResourcesAsync();
+             RepLaceViewAsync();
             await base.InstallAsync(context);
         }
 
@@ -30,6 +35,44 @@ namespace Smartstore.DevTools
             await DeleteSettingsAsync<ProfilerSettings>();
             await DeleteLanguageResourcesAsync();
             await base.UninstallAsync();
+        }
+
+        void RepLaceViewAsync()
+        {
+            var fileReplaces = new Dictionary<string, string>();
+            fileReplaces.Add("Views/Shared/Components/Footer/Default.cshtml", "Modules/Smartstore.DevTools/Views/Shared/Components/Footer/Default.cshtml");
+
+
+            foreach (var dict in fileReplaces)
+            {
+                var source = dict.Key.SplitSafe("/");
+                var desc = dict.Value.SplitSafe("/");
+                var arr = source.ToList();
+                arr.Insert(0, CommonHelper.ContentRoot.Root);
+
+                var sourceFileName = PathUtility.Combine(arr.ToArray());
+                // backup
+                {
+                    if (File.Exists(sourceFileName))
+                    {
+                        File.Copy(sourceFileName, sourceFileName + ".old", true);
+                    }
+                }
+                //replace
+                {
+                    var descArr = desc.ToList();
+                    descArr.Insert(0, CommonHelper.ContentRoot.Root);
+                    var fileName = PathUtility.Combine(descArr.ToArray());
+                    if (File.Exists(fileName))
+                    {
+                        File.Copy(fileName, sourceFileName, true);
+                    }
+                    else
+                    {
+                        File.Copy(sourceFileName + ".old", sourceFileName, true);
+                    }
+                }
+            }
         }
     }
 }
